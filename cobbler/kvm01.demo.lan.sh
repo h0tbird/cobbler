e6d9d5fcb90ce26bef78ba31d8f6d0f76a01cd44
Marc Villacorta <marc.villacorta@gmail.com>
2013-03-16 22:06:32 +0100

    Enable STP.

--- cobbler/kvm01.demo.lan.sh
+++ cobbler/kvm01.demo.lan.sh
@@ -19,8 +19,8 @@ dns=8.8.8.8
 cobbler system remove --name=$h
 cobbler system add  --name=$h --profile=$profile --hostname=$h --name-servers=$dns --gateway=$gw
 cobbler system edit --name=$h --interface=br0 --interface-type=bridge --bridge-opts="stp=no" --ip-address=$i0 --netmask=$n0 --static=1
-cobbler system edit --name=$h --interface=br1_3 --interface-type=bridge --bridge-opts="stp=no" --static=1
-cobbler system edit --name=$h --interface=br1_6 --interface-type=bridge --bridge-opts="stp=no" --static=1
+cobbler system edit --name=$h --interface=br1_3 --interface-type=bridge --bridge-opts="STP=yes" --static=1
+cobbler system edit --name=$h --interface=br1_6 --interface-type=bridge --bridge-opts="STP=yes" --static=1
 cobbler system edit --name=$h --interface=eth0 --mac=$m0 --interface-type=bridge_slave --interface-master=br0
 cobbler system edit --name=$h --interface=eth1.3 --interface-type=bridge_slave --interface-master=br1_3 --static=1
 cobbler system edit --name=$h --interface=eth1.6 --interface-type=bridge_slave --interface-master=br1_6 --static=1
