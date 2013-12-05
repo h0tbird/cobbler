e89fe6e55bf36e6a6d2691bd87c2a3043e572215
Marc Villacorta <marc.villacorta@gmail.com>
2013-12-06 00:09:43 +0100

    Adding new profiles

--- profiles/virtual.sh
+++ profiles/virtual.sh
@@ -4,7 +4,7 @@
 # Define:
 #---------
 
-name='Hardware'
+name='Virtual'
 distro='CentOS_6-x86_64'
 kickstart='/var/lib/cobbler/kickstarts/demo.ks'
 repos='CentOS_6-x86_64-base CentOS_6-x86_64-extras CentOS_6-x86_64-updates EPEL-el6-x86_64 EPEL_testing-el6-x86_64 puppetlabs-deps-el6-x86_64 puppetlabs-products-el6-x86_64'
